9ee68133cac661c82c4d537726d9d6ec7e99a024
Ivan Sergeev <ahrejlok2007@ya.ru>
2021-04-11 16:16:57 +0500

    change app

--- app.js
+++ app.js
@@ -8,6 +8,7 @@ const { errors } = require('celebrate');
 const { NODE_ENV, MONGO_DB } = process.env;
 const bodyParser = require('body-parser');
 const { requestLogger, errorLogger } = require('./middlewares/logger');
+const corsUrl = require('./middlewares/corsUrl');
 const errorHandler = require('./errors/errorHandler');
 const router = require('./routes/index.js');
 const mongoAd = require('./mongo/mongoAd');
@@ -15,13 +16,7 @@ const mongoAd = require('./mongo/mongoAd');
 const { PORT = 3000 } = process.env;
 const app = express();
 app.use(cors());
-app.use(function(req, res, next) {
-  res.header('Access-Control-Allow-Origin', 'http://localhost:8082');
-  res.header('Access-Control-Allow-Headers', 'Origin, X-Requested-With, authorization,Content-Type, Accept');
-  res.header('Access-Control-Allow-Methods', 'GET,HEAD,PUT,PATCH,POST,DELETE');
-
-  next();
-}); 
+app.use(corsUrl);
 app.use(helmet());
 app.use(requestLogger); // подключаем логгер запросов
 app.use(bodyParser.json());
@@ -38,5 +33,5 @@ app.use(errors()); // обработчик ошибок celebrate
 app.use(errorHandler);
 app.use(errorLogger); // подключаем логгер ошибок
 app.listen(PORT, () => {
-  
+
 });
